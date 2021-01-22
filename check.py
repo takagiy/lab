9f4cd03e9eec3051c2275184265fcdfe3b584f3a
takagiy <takagiy.4dev@gmail.com>
2021-01-23 01:30:57 +0900

    Fix hardcoded parameters

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ def isspanning(tree, startv = 0, visited = None):
     for neighbor in tree[startv]:
         visited.append(neighbor)
         isspanning(tree, startv = neighbor, visited = visited)
-    expected = list(range(20))
+    expected = list(range(len(tree)))
     return sorted(visited) == expected
 
 def isindependent(ists):
@@ -12,7 +12,7 @@ def isindependent(ists):
     anceses = [None] * 6
     for i in range(6):
         anceses[i] = collectances(ists[i])
-    for v in range(1, 20):
+    for v in range(1, len(ists[0])):
         for ti in range(6):
             for tj in range(ti + 1, 6):
                 commonances = set(anceses[ti][v]) & set(anceses[tj][v])
@@ -24,7 +24,7 @@ def isindependent(ists):
 
 def collectances(tree, parent = 0, ances = None):
     if ances == None:
-        ances = [[]] * 20
+        ances = [[]] * len(tree)
     for neighbor in tree[parent]:
         ances[neighbor] = ances[parent] + [parent]
         collectances(tree, parent = neighbor, ances = ances)
@@ -56,17 +56,17 @@ def verifynedges(n, d1, d2, count):
                 print('ERROR: Too many edge {} expected one but found {}'.format((u, v), count[u, v]))
 
 def main():
-    from istset0 import ists
+    from istset0 import ists, N, D1, D2
 
     for ti in range(6):
         ances = collectances(ists[ti])
-        for vi in range(20):
+        for vi in range(len(ists[0])):
             print('Ancestors of vertex {} in IST_{} : {}'.format(vi, ti, ances[vi]))
     for i in range(6):
         print('Is IST_{} spanning? : {}'.format(i, isspanning(ists[i])))
     print('Is all ISTs independent? : {}'.format(isindependent(ists)))
     nedges = countedges(ists)
-    verifynedges(20, 4, 8, nedges)
+    verifynedges(N, D1, D2, nedges)
 
 if __name__ == '__main__':
     main()

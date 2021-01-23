455eb9c8a8350ab8b3c06dd0970e9edfb013d6d9
takagiy <takagiy.4dev@gmail.com>
2021-01-23 23:53:47 +0900

    Increase number of test cases

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import check
 import ctor
 
-for D1 in range(3, 65):
+for D1 in range(3, 3 + 129):
     D2 = 2 * D1
-    for N in range(3 * D2, 3 * D2 + 65):
+    for N in range(3 * D2, 3 * D2 + 129):
         ists = ctor.makeists(N, D1, D2)
         print(f'====TEST CR({N}, {D1}, {D2})====')
         check.do_check(N, D1, D2, ists)
